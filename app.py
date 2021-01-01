57ba70237af33f76494caf6feb9daa734548c246
BuildTools <unconfigured@null.spigotmc.org>
2021-01-02 00:03:48 +0100

    fixed CREATE-Call

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import pathlib
-
 from tinydb import TinyDB, where
-from flask import Flask, request, render_template, flash, redirect, url_for
+
+from flask import Flask, request, render_template, redirect, url_for, make_response
 from werkzeug.utils import secure_filename
 
 import os
+import pathlib
 
 UPLOAD_FOLDER = os.getcwd() + "/functions"
 ALLOWED_EXTENSIONS = {"py", "js"}
@@ -20,65 +20,113 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/device/<name>', methods=['GET', 'DELETE'])
+@app.route('/device/<name>', methods=['GET', 'PUT', 'DELETE'])
 def device(name):
     if request.method == 'GET':
+        if devicesDB.search(where('name') == name) is None:
+            return {"message": "No device found"}, 404
+
         Device = devicesDB.search(where('name') == name)
-        return render_template("device.html", device=Device[0]), 200
-    elif request.method == 'DELETE':
-        devicesDB.delete(where('name') == name)
-        return {"message": "Device deleted"}, 200
+        resp = make_response(render_template("device.html", device=Device[0]))
+        resp.set_cookie("device", name)
+        return resp
+
+    if request.method == 'PUT':
+        updatedValues = request.get_json()
+        print(updatedValues)
+        devicesDB.update(set(updatedValues), where('name') == name)
+        return redirect(url_for('/'))
+
+    if request.method == 'DELETE':
+        devicesDB.remove(where('name') == name)
+        return redirect(url_for('/'))
+
     else:
-        return "Request  not allowed", 405
+        return "Request not allowed", 405
 
 
-@app.route('/function/upload/<name>', methods=['POST'])
-def upload_function(name):
+@app.route('/function/upload', methods=['POST'])
+def upload_function():
+    name = request.cookies.get("device")
+
     if request.method == 'POST':
-        if not pathlib.Path.exists(name):
-            os.mkdir(name)
+
+        if not pathlib.Path(os.path.join(UPLOAD_FOLDER, name)).exists():
+            os.mkdir(os.path.join(UPLOAD_FOLDER, name))
+            return {"message": "Folder for functions created."}, 200
+
         else:
+            print('2')
             # check if the post request has the file part
             if 'file' not in request.files:
-                flash('No file part')
-                return redirect(request.url)
+                # flash('No file part')
+                return redirect(url_for('list_functions'))
             file = request.files['file']
             # if user does not select file, browser also
             # submit an empty part without filename
             if file.filename == '':
-                flash('No selected file')
-                return redirect(request.url)
+                print('4')
+                # flash('No selected file')
+                return redirect(url_for('list_functions'))
             if file and allowed_file(file.filename):
+                print('5')
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], name, filename))
-                return redirect(url_for('device', name=name))
+                return redirect(url_for('list_functions'))
+
     else:
         return {"message": "Wrong method"}, 400
 
 
-@app.route('/function/<name>')
-def function(name):
+@app.route('/functions')
+def list_functions():
+    name = request.cookies.get("device")
+
     if devicesDB.search(where('name') == name) is None:
         return {"message": "No device found"}, 404
+
     else:
         from os import listdir
         from os.path import isfile, join
         script_path = join(UPLOAD_FOLDER, name)
         scripts = [f for f in listdir(script_path) if isfile(join(script_path, f))]
-        return render_template("function.html", scripts=scripts)
+        return render_template("functions.html", scripts=scripts)
+
+
+@app.route('/function/run')
+def run_function():
+    name = request.cookies.get("device")
+
+    if devicesDB.search(where('name') == name) is None:
+        return {"message": "No device found"}, 404
+
+    else:
+        """
+        interpreter for python and js scripts
+        """
+        return {"message": "under construction"}, 200
 
 
 @app.route('/', methods=['GET', 'POST'])
-def devices():
+def index():
     if request.method == 'GET':
         return render_template("devices.html", devices=devicesDB.all()), 200
-    elif request.method == 'POST':
-        content = request.get_json()
-        if devicesDB.search(where('name') == content['name']) is not None:
-            return {"message": "Device already created"}, 200
-        devicesDB.insert(content)
-        return {"message": "Device created"}, 201
+
+    if request.method == 'POST':
+
+        newDevice = {
+            "name": request.form['name'],
+            "type": request.form['type'],
+            "ip": request.form['ip']
+        }
+
+        if len(devicesDB.search(where('name') == newDevice['name'])) != 0:
+            return {"message": "Device already exists"}, 200
+
+        else:
+            devicesDB.insert(newDevice)
+            return {"message": "Device created"}, 201
 
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', 8080)
+    app.run(port=8080)
