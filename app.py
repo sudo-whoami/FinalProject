3144b76a1eeacfea90b8733e223d385ee71313c3
sudo-whoami <moritz.techen@gmail.com>
2021-01-18 20:53:27 +0100

    changes on function run

--- app.py
+++ app.py
@@ -1,31 +1,71 @@
 from tinydb import TinyDB, where
 
-from flask import Flask, request, render_template, redirect, url_for, make_response
+from flask import Flask, request, render_template, redirect, url_for, make_response, flash
 from werkzeug.utils import secure_filename
 
 import os
 import pathlib
+import subprocess
+import psutil
+
 
 UPLOAD_FOLDER = os.getcwd() + "/functions"
-ALLOWED_EXTENSIONS = {"py", "js"}
+ALLOWED_EXTENSIONS = {"py",}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = b'W-27$UU_#9B$74Nb'
+
 db = TinyDB('database.json')
 
 devicesDB = db.table('devices')
 
 
+class Script():
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.status = {}
+        self.p = subprocess.call(["python", self.filename], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+
+
+    def getStatus(self):
+
+        if not os.path.exists(self.filename):
+            self.status['message'] = "File not found"
+            self.status['code'] = 1
+
+        if not self.filename.split(".")[1] == "py":
+            self.status['message'] = "Not supported filetype"
+            self.status['code'] = 2
+
+        if self.p == 0:
+            self.status['message'] = "Script executed"
+            self.status['code'] = 3
+
+        elif self.p == 1:
+            self.status['message'] = "Script terminated"
+            self.status['code'] = 4
+
+        return self.status
+
+
+    def kill(self):
+
+        process = psutil.Process(self.p.pid)
+        for proc in process.children(recursive=True):
+            proc.kill()
+        process.kill()
+
+
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
 @app.route('/device/<name>', methods=['GET', 'PUT', 'DELETE'])
 def device(name):
-    if request.method == 'GET':
-        if devicesDB.search(where('name') == name) is None:
-            return {"message": "No device found"}, 404
 
+    if request.method == 'GET':
         Device = devicesDB.search(where('name') == name)
         resp = make_response(render_template("device.html", device=Device[0]))
         resp.set_cookie("device", name)
@@ -33,49 +73,45 @@ def device(name):
 
     if request.method == 'PUT':
         updatedValues = request.get_json()
-        print(updatedValues)
         devicesDB.update(set(updatedValues), where('name') == name)
-        return redirect(url_for('/'))
+        flash('Device edited', 'success')
+        return redirect('/'), 308
 
     if request.method == 'DELETE':
         devicesDB.remove(where('name') == name)
-        return redirect(url_for('/'))
-
-    else:
-        return "Request not allowed", 405
+        flash('Device deleted', 'success')
+        return redirect('/'), 308
 
 
-@app.route('/function/upload', methods=['POST'])
+@app.route('/function/upload', methods=['GET', 'POST'])
 def upload_function():
     name = request.cookies.get("device")
+
+    if request.method == 'GET':
+        return render_template('upload_function.html')
 
     if request.method == 'POST':
 
         if not pathlib.Path(os.path.join(UPLOAD_FOLDER, name)).exists():
             os.mkdir(os.path.join(UPLOAD_FOLDER, name))
-            return {"message": "Folder for functions created."}, 200
+            flash('Folder created. Please reupload', 'warning')
+            return redirect('/function/upload'), 308
 
         else:
-            print('2')
             # check if the post request has the file part
             if 'file' not in request.files:
-                # flash('No file part')
-                return redirect(url_for('list_functions'))
+                flash('No file part', 'warning')
+                return redirect('/functions'), 308
             file = request.files['file']
             # if user does not select file, browser also
             # submit an empty part without filename
             if file.filename == '':
-                print('4')
-                # flash('No selected file')
-                return redirect(url_for('list_functions'))
+                flash('No selected file', 'warning')
+                return redirect('/functions'), 308
             if file and allowed_file(file.filename):
-                print('5')
                 filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], name, filename))
-                return redirect(url_for('list_functions'))
-
-    else:
-        return {"message": "Wrong method"}, 400
+                file.save(os.path.join(UPLOAD_FOLDER, name, filename))
+                return redirect('/functions'), 308
 
 
 @app.route('/functions')
@@ -83,14 +119,18 @@ def list_functions():
     name = request.cookies.get("device")
 
     if devicesDB.search(where('name') == name) is None:
-        return {"message": "No device found"}, 404
+        flash('Device not found', 'error')
+        return redirect('/'), 308
 
     else:
-        from os import listdir
-        from os.path import isfile, join
-        script_path = join(UPLOAD_FOLDER, name)
-        scripts = [f for f in listdir(script_path) if isfile(join(script_path, f))]
-        return render_template("functions.html", scripts=scripts)
+        try:
+            from os import listdir
+            from os.path import isfile, join
+            script_path = join(UPLOAD_FOLDER, name)
+            scripts = [f for f in listdir(script_path) if isfile(join(script_path, f))]
+            return render_template("functions.html", scripts=scripts)
+        except:
+            return redirect('/function/upload')
 
 
 @app.route('/function/run')
@@ -98,19 +138,22 @@ def run_function():
     name = request.cookies.get("device")
 
     if devicesDB.search(where('name') == name) is None:
-        return {"message": "No device found"}, 404
+        flash('Device not found', 'error')
+        return redirect('/'), 308
 
     else:
         """
-        interpreter for python and js scripts
+        interpreter for python scripts
         """
-        return {"message": "under construction"}, 200
+        status = Script(os.path.join(UPLOAD_FOLDER, name, request.args.get('filename'))).getStatus()
+
+        return status
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return render_template("devices.html", devices=devicesDB.all()), 200
+        return render_template("devices.html", devices=devicesDB.all())
 
     if request.method == 'POST':
 
@@ -121,11 +164,17 @@ def index():
         }
 
         if len(devicesDB.search(where('name') == newDevice['name'])) != 0:
-            return {"message": "Device already exists"}, 200
+            flash('Device already exists', 'error')
+            return redirect('/')
+
+        elif not (all(newDevice.values())):
+            flash('You need to fill the device information', 'error')
+            return redirect('/')
 
         else:
             devicesDB.insert(newDevice)
-            return {"message": "Device created"}, 201
+            flash('Device created', 'success')
+            return redirect('/')
 
 
 if __name__ == "__main__":
