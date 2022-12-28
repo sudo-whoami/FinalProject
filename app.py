a373a13f44567f033e20a92ff414be891dfd7e3c
Moritz Techen <moritz.techen@gmail.com>
2022-12-28 17:45:01 +0100

    test

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from typing import NoReturn
-from tinydb import TinyDB, where
+from tinydb import TinyDB, where, Query
 from tinydb.operations import add, set
 
 from flask import Flask, request, render_template, redirect, url_for, make_response, flash
@@ -27,6 +27,13 @@ devicesDB = db.table('devices')
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def updateScripts(args):
+    def transform(name):
+        Device = devicesDB.search(where('name') == name)
+        Device['scripts'].append(args)
+        devicesDB.update(set(Device),where('name') == name)
+
+    return transform
 
 @app.route('/device/<name>', methods=['GET', 'PUT', 'DELETE'])
 def device(name):
@@ -40,12 +47,12 @@ def device(name):
     if request.method == 'PUT':
         updatedValues = request.get_json()
         print(updatedValues)
-        #devicesDB.update(set(updatedValues), where('name') == name)
-        return redirect('/'), 308
+        devicesDB.update(set(updatedValues), where('name') == name)
+        return redirect('/', code=302) 
 
     if request.method == 'DELETE':
         devicesDB.remove(where('name') == name)
-        return redirect('/'), 308
+        return redirect('/', code=302)
 
 
 @app.route('/script/upload', methods=['GET', 'POST'])
@@ -63,62 +70,71 @@ def upload_function():
             os.mkdir(os.path.join(UPLOAD_FOLDER, name))
             # check if the post request has the file part
             if 'file' not in request.files:
-                flash('No file part', 'warning')
-                return redirect('/scripts'), 308
+                return redirect('/scripts', code=302)
                 
             file = request.files['file']
             # if user does not select file, browser also
             # submit an empty part without filename
             if file.filename == '':
-                flash('No selected file', 'warning')
-                return redirect('/scripts'), 308
+                return redirect('/scripts', code=302)
 
             elif file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(UPLOAD_FOLDER, name, filename))
                 scriptData = {'scriptName': filename, 'uploadTime': uploadTime}    
                 devicesDB.update(add("scripts", scriptData), where('name') == name)
-                return redirect('/scripts'), 308
+                return redirect('/device/' + name, code=302)
+
+            else:
+               return redirect('/scripts', code=302)
 
         else:
             # check if the post request has the file part
             if 'file' not in request.files:
-                flash('No file part', 'warning')
-                return redirect('/scripts'), 308
+                return redirect('/scripts', code=302)
                 
             file = request.files['file']
             # if user does not select file, browser also
             # submit an empty part without filename
             if file.filename == '':
-                flash('No selected file', 'warning')
-                return redirect('/scripts'), 308
+                return redirect('/scripts', code=302)
 
             elif file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(UPLOAD_FOLDER, name, filename))
-                scriptData = {'scriptName': filename, 'uploadTime': uploadTime}    
+                scriptData = {'scriptName': filename, 'uploadTime': uploadTime}
+                print(scriptData) 
+                print(devicesDB.search(where('name') == name)[0]['scripts'])   
                 devicesDB.update(add("scripts", scriptData), where('name') == name)
-                return redirect('/scripts'), 308
+                return redirect('/scripts', code=302)
+
+            else:
+               return redirect('/scripts', code=302) 
 
 
-@app.route('/scripts')
+@app.route('/scripts', methods=['GET', 'POST'])
 def list_scripts():
     name = request.cookies.get("device")
 
-    if devicesDB.search(where('name') == name) is None:
-        flash('Device not found', 'error')
-        return redirect('/'), 308
+    if request.method == 'GET':
 
-    else:
-        try:
-            script_path = join(UPLOAD_FOLDER, name)
-            uploadDates = devicesDB.search(where('name') == name)[0]
-            scripts = zip([file for file in listdir(script_path) if isfile(join(script_path, file))], uploadDates)
-            return render_template("scripts.html", scripts=scripts)
-        except:
-            flash('No files uploaded', 'warning')
-            return redirect('/script/upload')
+        if devicesDB.search(where('name') == name) is None:
+            flash('Device not found', 'error')
+            return redirect('/', code=302)
 
+        else:
+            try:
+                script_path = join(UPLOAD_FOLDER, name)
+                uploadTimes = devicesDB.search(where('name') == name)[0]['scripts']['uploadTime']
+                #print(uploadDates)
+                scripts = [file for file in listdir(script_path) if isfile(join(script_path, file))]
+                return render_template("scripts.html", scripts=scripts, uploadTimes = uploadTimes)
+            except:
+                flash('No files uploaded', 'warning')
+                return redirect('/script/upload', code=302)
+
+    if request.method == 'POST':
+        return redirect('/', code=302)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -136,12 +152,12 @@ def index():
 
         if len(devicesDB.search(where('name') == newDevice['name'])) != 0:
             flash('Device already exists', 'error')
-            return redirect('/')
+            return redirect('/', code=302)
 
         else:
             devicesDB.insert(newDevice)
             flash('Device created', 'success')
-            return redirect('/')
+            return redirect('/', code=302)
 
 
 if __name__ == "__main__":
